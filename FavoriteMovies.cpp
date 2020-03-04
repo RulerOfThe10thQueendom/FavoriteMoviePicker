388665d4dd5ab6e42e8f32b34eb400f8b184dbf2
Ruler of the 10th Queendom <yourfriend@10thQueendom.com>
2020-03-03 22:34:36 -0500

    Add files via upload
    Initial upload

---
+++ FavoriteMovies.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << rand() % 4; 
+	// 0 - The 10th Kingdom
+	// 1 - Pride and Prejudice
+	// 2 - Anastasia
+    // 3 - Trolls
+
+}
